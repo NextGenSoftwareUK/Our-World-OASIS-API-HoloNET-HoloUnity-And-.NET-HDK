65258960fe4f86f06267530e3033e2bc3fd2fc57
David Ellams <davidellams@hotmail.com>
2020-06-17 01:31:58 +0100

    - Added Nutrition to KarmaTypes - Added UserController for authentication, etc.

--- NextGenSoftware.OASIS.API.Core/KarmaTypE.cs
+++ NextGenSoftware.OASIS.API.Core/KarmaTypE.cs
@@ -7,6 +7,7 @@ namespace NextGenSoftware.OASIS.API.Core
     public enum KarmaType
     {
         OurWorld,
+        Nutrition,
         SelfHelpImprovement,
         HelpingPeople,
         HelpingTheEnvironment,
---
+++ NextGenSoftware.OASIS.API.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace NextGenSoftware.OASIS.API.WebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = await _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}

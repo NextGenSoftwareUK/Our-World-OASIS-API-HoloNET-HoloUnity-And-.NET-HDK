e6ef06c099b93b377561d07b6d93147698242670
David Ellams <davidellams@hotmail.com>
2020-07-18 11:24:22 +0100

    - Added Hashgraph & Scuttlebutt OASIS Providers (WIP).

---
+++ NextGenSoftware.OASIS.API.Providers.HashgraphOASIS/HashgraphOASIS.cs
@@ -0,0 +1,53 @@
+﻿using NextGenSoftware.OASIS.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.EOSIOOASIS
+{
+    public class HashgraphOASIS : OASISStorageBase, IOASISStorage, IOASISNET
+    {
+        public HashgraphOASIS()
+        {
+            this.ProviderName = "HashgraphOASIS";
+            this.ProviderDescription = "Hashgraph Provider";
+            this.ProviderType = ProviderType.HashgraphOASIS;
+            this.ProviderCategory = ProviderCategory.StorageAndNetwork;
+        }
+
+        public List<IHolon> GetHolonsNearMe(HolonType Type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IPlayer> GetPlayersNearMe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<IProfile> LoadProfileAsync(string providerKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<IProfile> LoadProfileAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<IProfile> LoadProfileAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<IProfile> SaveProfileAsync(IProfile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<ISearchResults> SearchAsync(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

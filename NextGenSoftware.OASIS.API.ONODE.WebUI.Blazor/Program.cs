27f3181dd9fc9c7f47696d8e4e58bb941dc72452
David Ellams <davidellams@hotmail.com>
2020-10-17 20:35:31 +0100

    - Added NextGenSoftware.OASIS.API.ONODE.WebUI.Blazor Web Assembly Web UI to the OASIS API (will allow you to create/manage your avatar, view where your karma was earnt/lost, what OAPP's you have installed and much more! :)

---
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.Blazor/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace NextGenSoftware.OASIS.API.ONODE.WebUI.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("Local", options.ProviderOptions);
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}

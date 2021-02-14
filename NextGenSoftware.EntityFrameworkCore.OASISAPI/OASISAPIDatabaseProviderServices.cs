de67c0387c36d848dbcdda69f9a601a566d5df20
David Ellams <davidellams@hotmail.com>
2021-02-14 19:42:17 +0000

    - Started adding EntityFrameworkCore to the OASIS API/STAR ODK so can implement a OASIS Storage Provider for EFCore allowing a powerful easy to use ORM/absraction API to run over every OASIS Provider/Internet. :)

---
+++ NextGenSoftware.EntityFrameworkCore.OASISAPI/OASISAPIDatabaseProviderServices.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace NextGenSoftware.EntityFrameworkCore.OASISAPI
+{
+    public class OASISAPIDatabaseProviderServices : DatabaseProviderServices
+    {
+    }
+}

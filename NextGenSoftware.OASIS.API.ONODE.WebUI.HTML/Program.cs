73f87d41018157f4483d8b97159982017565120d
David Ellams <davidellams@hotmail.com>
2021-03-26 16:13:42 +0000

    - Added additional menu items to OASIS API ONODE.WebUI. - Removed old redundant OASIS API ONODE.WebUI..

--- NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/Program.cs
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/Program.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace NextGenSoftware.OASIS.API.ONODE.WebUI.HTML
+namespace NextGenSoftware.OASIS.API.ONODE.WebUI.HTML2
 {
     public class Program
     {

fea0a4eb30a57693ded150fec0c0847ae64e345d
David Ellams <davidellams@hotmail.com>
2021-04-05 17:22:28 +0100

    OASIS API Web UI - Tidying up code:
    - Restored removed menu js code and put in new main.js file.
    - Moved postman download files to new postman folder.
    - Fine tuned menu widths.
    - Moved login popup to new popups folder.

---
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/wwwroot/js/main.js
@@ -0,0 +1,40 @@
+﻿
+function showComingSoonMessage()
+{
+    Swal.fire({
+        title: 'Coming Soon',
+        text: 'This module is coming soon...',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+    })
+}
+
+function showCheckAPIMessage()
+{
+    Swal.fire({
+        title: 'UI Coming Soon',
+        text: 'You can use this functionality directly by accessing the OASIS API from the Developer menu.',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+    })
+}
+
+function showSTARMessage()
+{
+    Swal.fire({
+        title: 'STAR ODK Coming Soon.',
+        text: 'This is functionaliy that is built in the STAR ODK and will soon be released. You can then download STAR ODK from the Developer menu.',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+    })
+}
+
+function showOurWorldMessage()
+{
+    Swal.fire({
+        title: 'Our World Coming Soon.',
+        text: 'This is functionaliy that is built in the Unity Smartphone Prototype for Our World and will soon be released. You can then download Our World from either the Map or OAPP menu.',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+    })
+}

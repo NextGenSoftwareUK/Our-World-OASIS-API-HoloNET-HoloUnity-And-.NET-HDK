40d0d5322da24ec483d1c6e45cfb05fdd5e1344b
David Ellams <davidellams@hotmail.com>
2021-04-20 20:52:43 +0100

    STAR ODK - Added new .NET 5 Blazor Templates.

---
+++ NextGenSoftware.OASIS.STAR.Templates.Blazor6/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NextGenSoftware.OASIS.STAR.Templates.Blazor6.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.STAR.Templates.Blazor6.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}

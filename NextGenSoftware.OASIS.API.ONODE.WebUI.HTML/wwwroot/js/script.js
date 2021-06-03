4d983037211303de307191720ca53fdb481e2d53
Scott Boudreaux <r.scott.boudreaux@gmail.com>
2021-06-03 13:12:29 -0700

    Add files via upload

--- NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/wwwroot/js/script.js
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/wwwroot/js/script.js
@@ -1,21 +1,21 @@
-(function () {
-	//Login/Signup modal window - by CodyHouse.co
-	function ModalSignin(element) {
+(function(){
+    //Login/Signup modal window - by CodyHouse.co
+	function ModalSignin( element ) {
 		this.element = element;
 		this.blocks = this.element.getElementsByClassName('js-signin-modal-block');
-		this.switchers = this.element.getElementsByClassName('js-signin-modal-switcher')[0].getElementsByTagName('a');
+		this.switchers = this.element.getElementsByClassName('js-signin-modal-switcher')[0].getElementsByTagName('a'); 
 		this.triggers = document.getElementsByClassName('js-signin-modal-trigger');
 		this.hidePassword = this.element.getElementsByClassName('js-hide-password');
 		this.init();
 	};
 
-	ModalSignin.prototype.init = function () {
+	ModalSignin.prototype.init = function() {
 		var self = this;
 		//open modal/switch form
-		for (var i = 0; i < this.triggers.length; i++) {
-			(function (i) {
-				self.triggers[i].addEventListener('click', function (event) {
-					if (event.target.hasAttribute('data-signin')) {
+		for(var i =0; i < this.triggers.length; i++) {
+			(function(i){
+				self.triggers[i].addEventListener('click', function(event){
+					if( event.target.hasAttribute('data-signin') ) {
 						event.preventDefault();
 						self.showSigninForm(event.target.getAttribute('data-signin'));
 					}
@@ -24,116 +24,115 @@
 		}
 
 		//close modal
-		this.element.addEventListener('click', function (event) {
-			if (hasClass(event.target, 'js-signin-modal') || hasClass(event.target, 'js-close')) {
+		this.element.addEventListener('click', function(event){
+			if( hasClass(event.target, 'js-signin-modal') || hasClass(event.target, 'js-close') ) {
 				event.preventDefault();
-				removeClass(self.element, 'signin-modal--is-visible');
+				removeClass(self.element, 'cd-signin-modal--is-visible');
 			}
 		});
 		//close modal when clicking the esc keyboard button
-		document.addEventListener('keydown', function (event) {
-			(event.which == '27') && removeClass(self.element, 'signin-modal--is-visible');
+		document.addEventListener('keydown', function(event){
+			if (event == '27') {
+				removeClass(self.element, 'cd-signin-modal--is-visible');
+			}
 		});
 
 		//hide/show password
-		for (var i = 0; i < this.hidePassword.length; i++) {
-			(function (i) {
-				self.hidePassword[i].addEventListener('click', function (event) {
+		for(var i =0; i < this.hidePassword.length; i++) {
+			(function(i){
+				self.hidePassword[i].addEventListener('click', function(event){
 					self.togglePassword(self.hidePassword[i]);
 				});
 			})(i);
-		}
+		} 
 
 		//IMPORTANT - REMOVE THIS - it's just to show/hide error messages in the demo
-		this.blocks[0].getElementsByTagName('form')[0].addEventListener('submit', function (event) {
+		this.blocks[0].getElementsByTagName('form')[0].addEventListener('submit', function(event){
 			event.preventDefault();
 			self.toggleError(document.getElementById('signin-email'), true);
 		});
-		this.blocks[1].getElementsByTagName('form')[0].addEventListener('submit', function (event) {
+		this.blocks[1].getElementsByTagName('form')[0].addEventListener('submit', function(event){
 			event.preventDefault();
 			self.toggleError(document.getElementById('signup-username'), true);
 		});
 	};
 
-	ModalSignin.prototype.togglePassword = function (target) {
+	ModalSignin.prototype.togglePassword = function(target) {
 		var password = target.previousElementSibling;
-		('password' == password.getAttribute('type')) ? password.setAttribute('type', 'text'): password.setAttribute('type', 'password');
-		target.textContent = ('Hide' == target.textContent) ? 'Show' : 'Hide';
+		( 'password' == password.getAttribute('type') ) ? password.setAttribute('type', 'text') : password.setAttribute('type', 'password');
+		target.textContent = ( 'Hide' == target.textContent ) ? 'Show' : 'Hide';
 		putCursorAtEnd(password);
 	}
 
-	ModalSignin.prototype.showSigninForm = function (type) {
+	ModalSignin.prototype.showSigninForm = function(type) {
 		// show modal if not visible
-		!hasClass(this.element, 'signin-modal--is-visible') && addClass(this.element, 'signin-modal--is-visible');
+		!hasClass(this.element, 'cd-signin-modal--is-visible') && addClass(this.element, 'cd-signin-modal--is-visible');
 		// show selected form
-		for (var i = 0; i < this.blocks.length; i++) {
-			this.blocks[i].getAttribute('data-type') == type ? addClass(this.blocks[i], 'signin-modal__block--is-selected') : removeClass(this.blocks[i], 'signin-modal__block--is-selected');
+		for( var i=0; i < this.blocks.length; i++ ) {
+			this.blocks[i].getAttribute('data-type') == type ? addClass(this.blocks[i], 'cd-signin-modal__block--is-selected') : removeClass(this.blocks[i], 'cd-signin-modal__block--is-selected');
 		}
 		//update switcher appearance
 		var switcherType = (type == 'signup') ? 'signup' : 'login';
-		for (var i = 0; i < this.switchers.length; i++) {
-			this.switchers[i].getAttribute('data-type') == switcherType ? addClass(this.switchers[i], 'selected') : removeClass(this.switchers[i], 'selected');
-		}
+		for( var i=0; i < this.switchers.length; i++ ) {
+			this.switchers[i].getAttribute('data-type') == switcherType ? addClass(this.switchers[i], 'cd-selected') : removeClass(this.switchers[i], 'cd-selected');
+		} 
 	};
 
-	ModalSignin.prototype.toggleError = function (input, bool) {
+	ModalSignin.prototype.toggleError = function(input, bool) {    
 		// used to show error messages in the form
-		toggleClass(input, 'signin-modal__input--has-error', bool);
-		toggleClass(input.nextElementSibling, 'signin-modal__error--is-visible', bool);
+		toggleClass(input, 'cd-signin-modal__input--has-error', bool);
+		toggleClass(input.nextElementSibling, 'cd-signin-modal__error--is-visible', bool);
 	}
 
 	var signinModal = document.getElementsByClassName("js-signin-modal")[0];
-	if (signinModal) {
+	if( signinModal ) {
 		new ModalSignin(signinModal);
 	}
 
 	// toggle main navigation on mobile
 	var mainNav = document.getElementsByClassName('js-main-nav')[0];
-	if (mainNav) {
-		mainNav.addEventListener('click', function (event) {
-			if (hasClass(event.target, 'js-main-nav')) {
+	if(mainNav) {
+		mainNav.addEventListener('click', function(event){
+			if( hasClass(event.target, 'js-main-nav') ){
 				var navList = mainNav.getElementsByTagName('ul')[0];
-				toggleClass(navList, 'main-nav__list--is-visible', !hasClass(navList, 'main-nav__list--is-visible'));
-			}
+				toggleClass(navList, 'cd-main-nav__list--is-visible', !hasClass(navList, 'cd-main-nav__list--is-visible'));
+			} 
 		});
 	}
-
+	
 	//class manipulations - needed if classList is not supported
 	function hasClass(el, className) {
-		if (el.classList) return el.classList.contains(className);
-		else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
 	}
-
 	function addClass(el, className) {
 		var classList = className.split(' ');
-		if (el.classList) el.classList.add(classList[0]);
-		else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
-		if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
 	}
-
 	function removeClass(el, className) {
 		var classList = className.split(' ');
-		if (el.classList) el.classList.remove(classList[0]);
-		else if (hasClass(el, classList[0])) {
-			var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
-			el.className = el.className.replace(reg, ' ');
-		}
-		if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
 	}
-
 	function toggleClass(el, className, bool) {
-		if (bool) addClass(el, className);
+		if(bool) addClass(el, className);
 		else removeClass(el, className);
 	}
 
 	//credits http://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
 	function putCursorAtEnd(el) {
-		if (el.setSelectionRange) {
-			var len = el.value.length * 2;
-			el.focus();
-			el.setSelectionRange(len, len);
-		} else {
-			el.value = el.value;
-		}
+    	if (el.setSelectionRange) {
+      		var len = el.value.length * 2;
+      		el.focus();
+      		el.setSelectionRange(len, len);
+    	} else {
+      		el.value = el.value;
+    	}
 	};
 })();

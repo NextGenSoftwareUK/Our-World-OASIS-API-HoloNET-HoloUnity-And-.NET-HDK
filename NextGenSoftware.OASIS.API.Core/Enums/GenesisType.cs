50ee4c632f5924a6799d5120faba0b4efb7e6af3
David Ellams <davidellams@hotmail.com>
2021-06-22 17:47:54 +0100

    - Updated GeneisType enum (Can now create Moons, Planets, Stars, SolarSystems,Galxies & Universes)
    (NOTE: Stars can exist outside of a Solar System and a Galaxy so can be created seperateley).

--- NextGenSoftware.OASIS.API.Core/Enums/GenesisType.cs
+++ NextGenSoftware.OASIS.API.Core/Enums/GenesisType.cs
@@ -3,12 +3,12 @@ namespace NextGenSoftware.OASIS.API.Core.Enums
 {
     public enum GenesisType
     {
-        GreatGrandSuperStar, // Built-in and there will only ever be ONE of this within the OASIS. (Omiverse)
-        GrandSuperStar, //level 166 (max level for avatar?) ( Universe)
+        //GreatGrandSuperStar, // Built-in and there will only ever be ONE of this within the OASIS. (Omiverse)
+       // GrandSuperStar, //level 166 (max level for avatar?) ( Universe)
        // SuperStar, //level 133 (Galaxy) Cannot create a SuperStar on its own, you create a Galaxy which comes with a new SuperStar at the centre.
-        Star, //Level 99        
-        Planet, //Level 33
         Moon, //Level 1
+        Planet, //Level 33
+        Star, //Level 99        
         SoloarSystem, //Level 99        
         Galaxy, //level 133 (Galaxy)
         Universe //level 166 (max level for avatar?) ( Universe)

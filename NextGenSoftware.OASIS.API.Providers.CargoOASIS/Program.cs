800d89bf2d6d8d1e9becb4ed8e7fa892e3677d07
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-19 22:51:30 +0500

    created cargo provider project

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

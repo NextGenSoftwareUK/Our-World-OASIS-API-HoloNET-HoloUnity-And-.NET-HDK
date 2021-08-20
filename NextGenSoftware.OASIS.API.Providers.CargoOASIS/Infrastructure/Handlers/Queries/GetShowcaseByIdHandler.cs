f29c8c67162a2a885d7d062a8552ef690bc41b0d
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 18:15:58 +0500

    Create GetShowcaseByIdHandler.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetShowcaseByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
+{
+    public class GetShowcaseByIdHandler : IHandle<Response<GetShowcaseByIdResponseModel>, GetShowcaseByIdRequestModel>
+    {
+        public Task<Response<GetShowcaseByIdResponseModel>> Handle(GetShowcaseByIdRequestModel request)
+        {
+            
+        }
+    }
+
+    public class GetShowcaseByIdRequestModel
+    {
+    }
+
+    public class GetShowcaseByIdResponseModel
+    {
+    }
+}

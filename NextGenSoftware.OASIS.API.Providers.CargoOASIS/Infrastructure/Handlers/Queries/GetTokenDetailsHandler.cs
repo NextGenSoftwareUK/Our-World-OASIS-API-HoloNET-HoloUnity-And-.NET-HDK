5d66f1773dd8c5da2bb5e556812a602bfc5a1915
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 14:05:22 +0500

    Update GetTokenDetailsHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetTokenDetailsHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetTokenDetailsHandler.cs
@@ -26,6 +26,15 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
 
     public class GetTokenDetailsRequestModel
     {
+        /// <summary>
+        /// The ID of the project on Cargo.
+        /// This can be found in the URL bar when viewing the project on Cargo.
+        /// </summary>
+        public string ProjectId { get; set; }
+        /// <summary>
+        /// The ID of the collectible within the collection.
+        /// </summary>
+        public string CollectibleId { get; set; }
     }
 
     public class GetTokenDetailsResponseModel

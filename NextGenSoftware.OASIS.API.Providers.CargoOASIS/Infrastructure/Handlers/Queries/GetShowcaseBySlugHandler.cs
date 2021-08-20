075c1231b96ec412dbc960428f1c7228ce2b0e32
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 18:45:53 +0500

    Create GetShowcaseBySlugHandler.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetShowcaseBySlugHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
+{
+    public class GetShowcaseBySlugHandler : IHandle<Response<GetShowcaseBySlugResponseModel>, GetShowcaseBySlugRequestModel>
+    {
+        public Task<Response<GetShowcaseBySlugResponseModel>> Handle(GetShowcaseBySlugRequestModel request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class GetShowcaseBySlugRequestModel
+    {
+    }
+
+    public class GetShowcaseBySlugResponseModel
+    {
+    }
+}

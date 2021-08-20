abdfce17523fd549ed7dc765c41af63510dcc597
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 14:23:11 +0500

    Create GetResaleItemsHandler.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetResaleItemsHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Cargo;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
+{
+    public class GetResaleItemsHandler : IHandle<Response<GetShowcaseByIdResponse>, GetResaleItemsRequestModel>
+    {
+        public async Task<Response<GetShowcaseByIdResponse>> Handle(GetResaleItemsRequestModel request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class GetResaleItemsRequestModel
+    {
+    }
+}

56f9870cc3de9b58acd26877307995652dd89648
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 17:46:20 +0500

    Create GetContractsHandler.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetContractsHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
+{
+    public class GetContractsHandler : IHandle<Response<GetContractsResponseModel>, GetContractsRequestHandler>
+    {
+        public Task<Response<GetContractsResponseModel>> Handle(GetContractsRequestHandler request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class GetContractsRequestHandler
+    {
+    }
+
+    public class GetContractsResponseModel
+    {
+    }
+}

0b4f8bdbf19a44f0ffb5de08dfb714838a55ba20
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 18:19:36 +0500

    Update GetShowcaseByIdHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetShowcaseByIdHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetShowcaseByIdHandler.cs
@@ -14,6 +14,16 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
 
     public class GetShowcaseByIdRequestModel
     {
+        /// <summary>
+        /// Required. String. The ID of the showcase
+        /// </summary>
+        public string ShowcaseId { get; set; }
+        /// <summary>
+        /// Optional. Boolean.
+        /// If true you must be authenticated and your authentication token will be sent in with the request.
+        /// This will be required to get information about a private showcase
+        /// </summary>
+        public bool Auth { get; set; }
     }
 
     public class GetShowcaseByIdResponseModel

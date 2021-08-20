7e22af568a87b08da888d2622cdb6337290d06d7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-20 14:01:04 +0500

    Update GetTokenDetailsHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetTokenDetailsHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetTokenDetailsHandler.cs
@@ -1,7 +1,34 @@
-﻿namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
 {
-    public class GetTokenDetailsHandler
+    public class GetTokenDetailsHandler : IHandle<GetTokenDetailsResponseModel, GetTokenDetailsRequestModel>
     {
+        private readonly HttpClient _httpClient;
+
+        public GetTokenDetailsHandler()
+        {
+            _httpClient = new HttpClient()
+            {
+                Timeout = TimeSpan.FromMinutes(1),
+                BaseAddress = new Uri("https://api2.cargo.build/")
+            };
+        }
         
+        public Task<GetTokenDetailsResponseModel> Handle(GetTokenDetailsRequestModel request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class GetTokenDetailsRequestModel
+    {
+    }
+
+    public class GetTokenDetailsResponseModel
+    {
     }
 }

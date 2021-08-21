ef13e66abf9735e008209ec691c56fbb601c1eb9
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-21 17:31:43 +0500

    Update GetAllUserCollectiblesHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetAllUserCollectiblesHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetAllUserCollectiblesHandler.cs
@@ -1,12 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Cargo;
 
 namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Queries
 {
-    public class GetAllUserCollectiblesHandler : IHandle<PaginationResponseWithResults<GetAllUserCollectiblesResponseModel>, GetAllUserCollectiblesRequestModel>
+    public class GetAllUserCollectiblesHandler : IHandle<PaginationResponseWithResults<IEnumerable<GetAllUserCollectiblesResponseModel>>, GetAllUserCollectiblesRequestModel>
     {
-        public Task<PaginationResponseWithResults<GetAllUserCollectiblesResponseModel>> Handle(GetAllUserCollectiblesRequestModel request)
+        public Task<PaginationResponseWithResults<IEnumerable<GetAllUserCollectiblesResponseModel>>> Handle(GetAllUserCollectiblesRequestModel request)
         {
             throw new System.NotImplementedException();
         }
@@ -14,6 +15,35 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
 
     public class GetAllUserCollectiblesResponseModel
     {
+        /*results: {
+        tokenId: string;
+        metadata: Record<string, any>;
+        tokenURI: string;
+        resaleItem?: {
+            sellerAddress: string;
+            tokenAddress: string;
+            tokenId: string;
+            resaleItemId: string;
+            price: string;
+            fromVendor: boolean;
+            metadata: Record<string, any>;
+        };
+        owner: string;
+        collection: {
+            _id: string;
+            address: string;
+            name: string;
+            symbol: string;
+            supportsMetadata: boolean;
+            tags?: string[];
+            owned?: boolean;
+            totalOwned?: number;
+            createdAt: string;
+        };
+   }[];
+   page: string;
+   limit: string;
+   totalPages: string;*/
     }
 
     public class GetAllUserCollectiblesRequestModel

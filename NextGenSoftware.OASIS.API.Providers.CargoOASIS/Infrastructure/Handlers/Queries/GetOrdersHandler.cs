2628c8c550cb95669f099e6acb2f57ed45027949
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-21 17:13:00 +0500

    Update GetOrdersHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetOrdersHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetOrdersHandler.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Cargo;
@@ -8,6 +11,19 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
 {
     public class GetOrdersHandler : IHandle<Response<PaginationResponseWithResults<IEnumerable<Order>>>, OrderParams>
     {
+        private readonly HttpClient _httpClient;
+        private readonly string _accessToken = string.Empty;
+
+        public GetOrdersHandler()
+        {
+            _httpClient = new HttpClient()
+            {
+                Timeout = TimeSpan.FromMinutes(1),
+                BaseAddress = new Uri("https://api2.cargo.build/")
+            };
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+        }
+        
         public Task<Response<PaginationResponseWithResults<IEnumerable<Order>>>> Handle(OrderParams request)
         {
             throw new System.NotImplementedException();

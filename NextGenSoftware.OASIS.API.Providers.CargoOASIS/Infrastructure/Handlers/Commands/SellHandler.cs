86cbb02f7f94bf582bcfb85f67302b47630c1141
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-22 22:29:09 +0500

    Create SellHandler.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Commands/SellHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Commands
+{
+    public class SellHandler : IHandle<Response<SellResponseModel>, SellRequestModel>
+    {
+        public SellHandler()
+        {
+            
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public Task<Response<SellResponseModel>> Handle(SellRequestModel request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class SellResponseModel
+    {
+    }
+
+    public class SellRequestModel
+    {
+    }
+}

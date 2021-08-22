c52e3aa428b129904e3e08d77d7828097b039204
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-22 21:46:37 +0500

    new interface created

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Commands/AuthenticateAccountHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers.Commands
+{
+    public class AuthenticateAccountHandler : ISingleHandler<Response<CreateAccountResponseModel>>
+    {
+        public AuthenticateAccountHandler()
+        {
+            
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public Task<Response<CreateAccountResponseModel>> Handle()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

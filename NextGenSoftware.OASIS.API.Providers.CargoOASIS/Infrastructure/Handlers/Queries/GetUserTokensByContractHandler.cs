d5a82610b775adfd11681fafa5720a51aacf31aa
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-22 22:52:58 +0500

    Update GetUserTokensByContractHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetUserTokensByContractHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetUserTokensByContractHandler.cs
@@ -54,6 +54,12 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
                     _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
                 }
                 var httpResponse = await _httpClient.SendAsync(httRequest);
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    response.Message = httpResponse.ReasonPhrase;
+                    response.ResponseStatus = ResponseStatus.Fail;
+                    return response;
+                }
                 var responseString = await httpResponse.Content.ReadAsStringAsync();
                 var data = JsonConvert.DeserializeObject<GetUserTokensByContractResponseModel>(responseString);
                 response.Payload = data;

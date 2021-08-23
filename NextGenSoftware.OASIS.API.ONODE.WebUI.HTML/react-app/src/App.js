4bc2409336eeecc64d57015c8641e2c526e2afc4
Rahul Gupta <rahuljandial2@gmail.com>
2021-08-23 22:07:17 +0530

    Added React Boiler plate code
    Added Boiler plate code for react and also copied the images folder used in the project

---
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./public/img/dummy-logo.svg" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

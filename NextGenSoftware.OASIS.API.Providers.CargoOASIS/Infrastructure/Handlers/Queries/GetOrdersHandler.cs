ac4e353231c86817e462d129736f729f86a9bf4d
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-23 11:17:27 +0500

    Update GetOrdersHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetOrdersHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Queries/GetOrdersHandler.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Enum;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Builder;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Exceptions;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.TokenStorage;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Cargo;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
@@ -15,8 +18,7 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
     public class GetOrdersHandler : IHandle<Response<PaginationResponseWithResults<IEnumerable<Order>>>, OrderParams>
     {
         private readonly HttpClient _httpClient;
-        private readonly string _accessToken = string.Empty;
-
+        private readonly ITokenStorage _tokenStorage;
         public GetOrdersHandler()
         {
             _httpClient = new HttpClient()
@@ -24,7 +26,7 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
                 Timeout = TimeSpan.FromMinutes(1),
                 BaseAddress = new Uri("https://api2.cargo.build/")
             };
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+            _tokenStorage = TokenStorageFactory.GetMemoryCacheTokenStorage();
         }
         
         /// <summary>
@@ -55,7 +57,11 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
                     Method = HttpMethod.Get,
                     RequestUri = new Uri(_httpClient.BaseAddress + urlQuery)
                 };
+                var accessToken = await _tokenStorage.GetToken();
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                 var httpResponse = await _httpClient.SendAsync(httRequest);
+                if(httpResponse.StatusCode == HttpStatusCode.Unauthorized)
+                    throw new UserNotAuthorizedException();
                 if (!httpResponse.IsSuccessStatusCode)
                 {
                     response.Message = httpResponse.ReasonPhrase;
@@ -65,6 +71,18 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Handlers
                 var responseString = await httpResponse.Content.ReadAsStringAsync();
                 var data = JsonConvert.DeserializeObject<PaginationResponseWithResults<IEnumerable<Order>>>(responseString);
                 response.Payload = data;
+                return response;
+            }
+            catch (UserNotAuthorizedException e)
+            {
+                response.ResponseStatus = ResponseStatus.Unauthorized;
+                response.Message = e.Message;
+                return response;
+            }
+            catch (UserNotRegisteredException e)
+            {
+                response.ResponseStatus = ResponseStatus.NotRegistered;
+                response.Message = e.Message;
                 return response;
             }
             catch (Exception e)

662da3d80042858747a80b5c203b5736b8d0bc4d
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-23 09:19:52 +0500

    token storage created

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/TokenStorage/ITokenStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.TokenStorage
+{
+    public interface ITokenStorage
+    {
+        Task SetTaken(string token);
+        Task<string> GetToken(string token);
+    }
+}
---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/TokenStorage/MemoryCacheTokenStorage.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.TokenStorage
+{
+    public class MemoryCacheTokenStorage : ITokenStorage
+    {
+        public Task SetTaken(string token)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> GetToken(string token)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

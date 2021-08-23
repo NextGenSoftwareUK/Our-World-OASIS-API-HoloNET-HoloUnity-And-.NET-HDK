f0936f93e2fa4e6f71ecd2321c2b5aa4d04c536f
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-23 11:32:26 +0500

    Update Cargo.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Singleton/Cargo.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Singleton/Cargo.cs
@@ -2,17 +2,11 @@
 {
     public sealed class Cargo
     {
-        private string _accessToken = string.Empty;
         private static Cargo _cargo;
         
         public static Cargo GetInstance()
         {
             return _cargo ??= new Cargo();
-        }
-
-        public void SetAccessToken(string token)
-        {
-            _accessToken = token;
         }
     }
 }

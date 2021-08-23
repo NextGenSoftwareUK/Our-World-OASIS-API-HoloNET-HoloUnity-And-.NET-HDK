abe28458b92a9d1ca7086a4d5e207c5301c0f7b6
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-23 10:01:30 +0500

    Update CreateAccountHandler.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Commands/CreateAccountHandler.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Handlers/Commands/CreateAccountHandler.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Enum;
-using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.TokenStorage;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Interfaces;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Models.Common;
 

20cc6a48ecc2733bd09130b6ba001be1c0674a82
David Ellams <davidellams@hotmail.com>
2021-08-24 20:28:49 +0100

    - Bug fixing on HoloNET RSM, getting closer... ;-)

--- NextGenSoftware.Holochain.HoloNET.Client.Core/HoloNETData.cs
+++ NextGenSoftware.Holochain.HoloNET.Client.Core/HoloNETData.cs
@@ -5,25 +5,37 @@ using System;
 namespace NextGenSoftware.Holochain.HoloNET.Client.Core
 {
     [MessagePackObject]
+    public struct Data
+    {
+        [Key(0)]
+        public byte[][] cell_id { get; set; } //= new byte[2][];
+        //public UInt32[] cell_id { get; set; } //= new byte[][1, 2]; //CellId = [HoloHash, AgentPubKey] = [string, string] = 2 dimensional array.
+
+        [Key(1)]
+        public string zome_name { get; set; }
+
+        [Key(2)]
+        public string fn_name { get; set; }
+
+        [Key(3)]
+        public byte[] payload { get; set; } //Payload - What is Payload object?
+
+        [Key(4)]
+        //public string cap { get; set; } //CapSecret | null = string
+        public byte[] cap { get; set; } //CapSecret | null = string
+
+        [Key(5)]
+        public byte[] provenance { get; set; } //AgentPubKey = string
+        //public string provenance { get; set; } //AgentPubKey = string
+    }
+
+    [MessagePackObject]
     public class HoloNETData
     {
         [Key(0)]
-        public string cap { get; set; } //CapSecret | null = string
+        public string type { get; set; }
 
         [Key(1)]
-        public byte[][] cell_id { get; set; } = new byte[2][]; 
-        //public UInt32[] cell_id { get; set; } //= new byte[][1, 2]; //CellId = [HoloHash, AgentPubKey] = [string, string] = 2 dimensional array.
-
-        [Key(2)]
-        public string zome_name { get; set; }
-
-        [Key(3)]
-        public string fn_name { get; set; }
-
-        [Key(4)]
-        public byte[] payload { get; set; } //Payload - What is Payload object?
-
-        [Key(5)]
-        public string provenance { get; set; } //AgentPubKey = string
+        public Data data { get; set; }
     }
 }

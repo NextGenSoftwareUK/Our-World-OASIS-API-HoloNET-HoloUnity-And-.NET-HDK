8d5ba4acea83aae4251fbc674c1b9755cb72d5d6
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-24 12:40:49 +0500

    signature changed

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/SignatureProviders/ISignatureProvider.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/SignatureProviders/ISignatureProvider.cs
@@ -4,6 +4,10 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.
 {
     public interface ISignatureProvider
     {
-        Task<string> GetSignature();
+        /// <summary>
+        /// Gets or generate signature
+        /// </summary>
+        /// <returns>Boolean - is there an error (true - yes, false - no), string - signature, if there are an error string will contain error message</returns>
+        Task<(bool, string)> GetSignature();
     }
 }
--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/SignatureProviders/MemoryCahceSignatureProvider.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/SignatureProviders/MemoryCahceSignatureProvider.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Exceptions;
 using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.TokenStorage;
 
 namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.SignatureProviders
 {
@@ -8,23 +11,38 @@ namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.
     {
         private readonly IMemoryCache _memoryCache;
         private readonly IConfigurationProvider _configuration;
+        private readonly ITokenStorage _tokenStorage;
         private readonly string _key;
 
         public MemoryCacheSignatureProvider()
         {
             _memoryCache = new MemoryCache(new MemoryCacheOptions());
             _configuration = ConfigurationFactory.GetLocalStorageConfigurationProvider();
+            _tokenStorage = TokenStorageFactory.GetMemoryCacheTokenStorage();
             _key = "_signature";
         }
 
-        public async Task<string> GetSignature()
+        public async Task<(bool, string)> GetSignature()
         {
-            var singingMessage = await _configuration.GetKey("singingMessage");
-            var token = _memoryCache.Get(_key);
-            if (token != null) return token.ToString();
-            token = "dfdf";
-            _memoryCache.Set(_key, token);
-            return token.ToString();
+            try
+            {
+                var singingMessage = await _configuration.GetKey("singingMessage");
+                var token = await _tokenStorage.GetToken();
+            
+                var signature = _memoryCache.Get(_key);
+                if (signature != null) return (false, signature.ToString());
+                signature = "dfdf";
+                _memoryCache.Set(_key, signature);
+                return (false, signature.ToString());
+            }
+            catch (UserNotRegisteredException e)
+            {
+                return (true, e.Message);
+            }
+            catch (Exception e)
+            {
+                return (true, e.Message);
+            }
         }
     }
 }

b03457a121940461c7155f63de65d2c6cdc70b0f
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-24 12:31:33 +0500

    Update MemoryCahceSignatureProvider.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/SignatureProviders/MemoryCahceSignatureProvider.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/SignatureProviders/MemoryCahceSignatureProvider.cs
@@ -1,30 +1,30 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
+using NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider;
 
 namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.SignatureProviders
 {
     public class MemoryCacheSignatureProvider : ISignatureProvider
     {
         private readonly IMemoryCache _memoryCache;
+        private readonly IConfigurationProvider _configuration;
         private readonly string _key;
-        private readonly string _singingMessage;
 
         public MemoryCacheSignatureProvider()
         {
             _memoryCache = new MemoryCache(new MemoryCacheOptions());
+            _configuration = ConfigurationFactory.GetLocalStorageConfigurationProvider();
             _key = "_signature";
         }
 
         public async Task<string> GetSignature()
         {
-            return await Task.Run(() =>
-            {
-                var token = _memoryCache.Get(_key);
-                if (token != null) return token.ToString();
-                token = "dfdf";
-                _memoryCache.Set(_key, token);
-                return token.ToString();
-            });        
+            var singingMessage = await _configuration.GetKey("singingMessage");
+            var token = _memoryCache.Get(_key);
+            if (token != null) return token.ToString();
+            token = "dfdf";
+            _memoryCache.Set(_key, token);
+            return token.ToString();
         }
     }
 }

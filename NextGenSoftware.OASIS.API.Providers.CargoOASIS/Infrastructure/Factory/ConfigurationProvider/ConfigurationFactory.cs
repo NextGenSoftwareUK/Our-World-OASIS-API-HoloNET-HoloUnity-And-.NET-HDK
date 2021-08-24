7bf7774591fc0c18a4b93207fd1e9bdb48ae0de0
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-24 12:28:31 +0500

    Create ConfigurationFactory.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/ConfigurationProvider/ConfigurationFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider
+{
+    public static class ConfigurationFactory
+    {
+        public static IConfigurationProvider GetLocalStorageConfigurationProvider()
+        {
+            return new LocalStorageConfigurationProvider();
+        }
+    }
+}

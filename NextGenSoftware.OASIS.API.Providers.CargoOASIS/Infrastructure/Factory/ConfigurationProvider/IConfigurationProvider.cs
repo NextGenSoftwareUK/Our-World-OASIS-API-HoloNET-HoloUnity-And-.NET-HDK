4272a44843d3729ca39b50cc0cd8841841fee74b
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-24 12:01:15 +0500

    Update IConfigurationProvider.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/ConfigurationProvider/IConfigurationProvider.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/ConfigurationProvider/IConfigurationProvider.cs
@@ -1,7 +1,10 @@
-﻿namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider
+﻿using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider
 {
     public interface IConfigurationProvider
     {
-        
+        Task SetKey(string key, object value);
+        Task<object> GetKey(string key);
     }
 }

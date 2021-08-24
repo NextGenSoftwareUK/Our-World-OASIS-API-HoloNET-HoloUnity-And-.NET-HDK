10c0dd0f8e1fd9b24733fa85963ce5fb6fa4d673
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-24 12:05:18 +0500

    Update LocalStorageConfigurationProvider.cs

--- NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/ConfigurationProvider/LocalStorageConfigurationProvider.cs
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/ConfigurationProvider/LocalStorageConfigurationProvider.cs
@@ -1,7 +1,17 @@
-﻿namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider
+﻿using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider
 {
-    public class LocalStorageConfigurationProvider
+    public class LocalStorageConfigurationProvider : IConfigurationProvider
     {
-        
+        public async Task SetKey(string key, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<object> GetKey(string key)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }

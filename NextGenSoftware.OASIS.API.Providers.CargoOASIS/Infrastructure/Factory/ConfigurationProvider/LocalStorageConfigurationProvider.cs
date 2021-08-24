9199184ae1f2f8008254099af6520986b1acaa54
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-24 12:04:51 +0500

    Create LocalStorageConfigurationProvider.cs

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS/Infrastructure/Factory/ConfigurationProvider/LocalStorageConfigurationProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.Infrastructure.Factory.ConfigurationProvider
+{
+    public class LocalStorageConfigurationProvider
+    {
+        
+    }
+}

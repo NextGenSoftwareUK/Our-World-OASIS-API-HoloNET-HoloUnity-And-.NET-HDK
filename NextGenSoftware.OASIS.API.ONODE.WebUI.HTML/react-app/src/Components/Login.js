4471249236caad2c5d16f7023ad4f9ed0d137424
Rahul Gupta <rahuljandial2@gmail.com>
2021-08-30 14:14:28 +0530

    Completed react app project
    - Made everything responsive
    - Added api functions to login/signup

--- NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/Components/Login.js
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/Components/Login.js
@@ -2,6 +2,43 @@ import React, { Component } from 'react'
 import '../CSS/Login.css'
 
 export default class Login extends Component {
+  onLogin(event) {
+    event.preventDefault();
+    let email = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+    let userObject = {
+      email,
+      password
+    }
+    const userAction = async () => {
+      const response = await fetch('https://api.oasisplatform.world/api/avatar/authenticate', {
+        method: 'POST',
+        body: JSON.stringify(userObject), // string or object
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.status === 200) {
+        const myJson = await response.json(); //extract JSON from the http response
+        alert(myJson.message);
+
+        // hide the login/signup buttons
+        var elementList = document.getElementsByClassName("nav-logins");
+        for (var i = 0; i < elementList.length; i++) {
+          elementList[i].classList.add('hide-logins');
+        }
+        //===============================//
+
+        window.location.reload();
+      } else {
+        const myJson = await response.json(); //extract JSON from the http response
+        alert(myJson.title);
+        window.location.reload();
+      }
+
+    }
+    userAction();
+  }
   render() {
     return (
       <form className="login-form">
@@ -12,16 +49,16 @@ export default class Login extends Component {
           </p>
         </div>
         <div className="login-inputs">
-          <label htmlFor="email">EMAIL</label>
-          <input type="email" placeholder="name@example.com" />
-          <label htmlFor="password">PASSWORD</label>
-          <input type="password" />
+          <label htmlFor="login-email">EMAIL</label>
+          <input id="login-email" type="email" placeholder="name@example.com" />
+          <label htmlFor="login-password">PASSWORD</label>
+          <input type="password" id="login-password" />
           <label className="link">Forgot Password?</label>
           <div>
             <input type="checkbox" name="remember-login" id="remember-login" />
             <label htmlFor="remember-login">Remember Me</label>
           </div>
-          <button type="submit" className="login-submit">Submit</button>
+          <button onClick={this.onLogin} className="login-submit">Submit</button>
         </div>
       </form>
     )

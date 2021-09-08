81b5248d47114a75beca229b5391ef71359b1172
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-08 22:12:29 +0500

    added requests and results models

--- NextGenSoftware.OASIS.API.Providers.SOLANAOASIS/Infrastructure/Services/Solana/ISolanaService.cs
+++ NextGenSoftware.OASIS.API.Providers.SOLANAOASIS/Infrastructure/Services/Solana/ISolanaService.cs
@@ -1,7 +1,63 @@
-﻿namespace NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.Infrastructure.Services.Solana
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.Infrastructure.Models.Common;
+
+namespace NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.Infrastructure.Services.Solana
 {
     public interface ISolanaService
     {
-        
+        Task<Response<ExchangeTokenResult>> ExchangeTokens(ExchangeTokenRequest exchangeTokenRequest);
+        Task<Response<ExchangeNftResult>> ExchangeNft(ExchangeNftRequest exchangeNftRequest);
+        Task<Response<MintNftResult>> MintNft(MintNftRequest mintNftRequest);
+        Task<Response<SendTransactionResult>> SendTransaction(SendTransactionRequest sendTransactionRequest);
+        Task<Response<GetNftMetadataResult>> GetNftMetadata(GetNftMetadataRequest getNftMetadataRequest);
+        Task<Response<GetNftWalletResult>> GetNftWallet(GetNftWalletRequest getNftWalletRequest);
+    }
+
+    public class GetNftWalletRequest
+    {
+    }
+
+    public class GetNftWalletResult
+    {
+    }
+
+    public class GetNftMetadataRequest
+    {
+    }
+
+    public class GetNftMetadataResult
+    {
+    }
+
+    public class SendTransactionRequest
+    {
+    }
+
+    public class SendTransactionResult
+    {
+    }
+
+    public class MintNftRequest
+    {
+    }
+
+    public class MintNftResult
+    {
+    }
+
+    public class ExchangeNftRequest
+    {
+    }
+
+    public class ExchangeNftResult
+    {
+    }
+
+    public class ExchangeTokenRequest
+    {
+    }
+
+    public class ExchangeTokenResult
+    {
     }
 }

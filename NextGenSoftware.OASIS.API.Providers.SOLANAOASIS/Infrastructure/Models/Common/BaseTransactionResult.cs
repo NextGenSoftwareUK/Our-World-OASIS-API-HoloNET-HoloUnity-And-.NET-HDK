098517f44f1aea46975f47a4020f3c745322ca62
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-10 18:14:48 +0500

    Create BaseTransactionResult.cs

---
+++ NextGenSoftware.OASIS.API.Providers.SOLANAOASIS/Infrastructure/Models/Common/BaseTransactionResult.cs
@@ -0,0 +1,12 @@
+﻿namespace NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.Infrastructure.Models.Common
+{
+    public abstract class BaseTransactionResult
+    {
+        public string TransactionResult { get; set; }
+
+        public BaseTransactionResult(string transactionResult)
+        {
+            TransactionResult = transactionResult;
+        }
+    }
+}

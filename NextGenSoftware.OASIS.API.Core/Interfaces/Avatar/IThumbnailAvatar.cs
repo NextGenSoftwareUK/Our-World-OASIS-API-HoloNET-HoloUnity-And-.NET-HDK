5139e07511d905c8668532b2241d87e507820e02
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-15 18:24:58 +0500

    Update IThumbnailAvatar.cs

---
+++ NextGenSoftware.OASIS.API.Core/Interfaces/Avatar/IThumbnailAvatar.cs
@@ -0,0 +1,12 @@
+﻿namespace NextGenSoftware.OASIS.API.Core.Interfaces
+{
+    public interface IThumbnailAvatar
+    {
+        string Username { get; set; }
+        string Password { get; set; }
+        string Email { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string FullName { get; }
+    }
+}

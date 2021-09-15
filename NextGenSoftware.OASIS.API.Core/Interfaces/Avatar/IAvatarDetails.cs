b0e39736c13efa3f0870eb41ef6f9f5f13b7e983
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-15 19:43:27 +0500

    added thumbnail and detail object

---
+++ NextGenSoftware.OASIS.API.Core/Interfaces/Avatar/IAvatarDetails.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NextGenSoftware.OASIS.API.Core.Enums;
+using NextGenSoftware.OASIS.API.Core.Helpers;
+using NextGenSoftware.OASIS.API.Core.Interfaces.STAR;
+using NextGenSoftware.OASIS.API.Core.Objects;
+
+namespace NextGenSoftware.OASIS.API.Core.Interfaces
+{
+    public interface IAvatarDetails : IHolon 
+    {
+        string Image2D { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+        string Email { get; set; }
+        string Title { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        ConsoleColor FavouriteColour { get; set; }
+        ConsoleColor StarcliColour { get; set; }
+        string FullName { get; }
+        DateTime Dob { get; set; }
+        string Address { get; set; }
+        int Karma { get; set; }
+        int Level { get; }
+        int Xp { get; set; }
+        IOmiverse Omiverse { get; set; }
+        List<AvatarGift> Gifts { get; set; }
+        AvatarChakras Chakras { get; set; }
+        AvatarAura Aura { get; set; }
+        AvatarStats Stats { get; set; }
+        List<GeneKey> GeneKeys { get; set; } 
+        HumanDesign HumanDesign { get; set; } 
+        AvatarSkills Skills { get; set; } 
+        AvatarAttributes Attributes { get; set; }
+        AvatarSuperPowers SuperPowers { get; set; } 
+        List<Spell> Spells { get; set; } 
+        List<Achievement> Achievements { get; set; } 
+        List<InventoryItem> Inventory { get; set; } 
+        string Town { get; set; }
+        string County { get; set; }
+        string Country { get; set; }
+        string Postcode { get; set; }
+        string Mobile { get; set; }
+        string Landline { get; set; }
+        EnumValue<AvatarType> AvatarType { get; set; }
+        EnumValue<OASISType> CreatedOasisType { get; set; }
+        bool AcceptTerms { get; set; }
+        public string VerificationToken { get; set; }
+        DateTime? Verified { get; set; }
+        bool IsVerified => Verified.HasValue || PasswordReset.HasValue;
+        string ResetToken { get; set; }
+        string JwtToken { get; set; }
+        string RefreshToken { get; set; }
+        DateTime? ResetTokenExpires { get; set; }
+        DateTime? PasswordReset { get; set; }
+    }
+}
--- NextGenSoftware.OASIS.API.Core/Interfaces/Avatar/IAvatarThumbnail.cs
+++ NextGenSoftware.OASIS.API.Core/Interfaces/Avatar/IAvatarThumbnail.cs
@@ -1,10 +1,14 @@
-﻿namespace NextGenSoftware.OASIS.API.Core.Interfaces
+﻿using System;
+
+namespace NextGenSoftware.OASIS.API.Core.Interfaces
 {
-    public interface IThumbnailAvatar
+    public interface IAvatarThumbnail
     {
+        Guid Id { get; set; }
         string Username { get; set; }
         string Password { get; set; }
         string Email { get; set; }
+        string Title { get; set; }
         string FirstName { get; set; }
         string LastName { get; set; }
         string FullName { get; }

1dddd36ff5288dc15a1340f993d8ac1a0ffe83ef
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-16 22:25:52 +0500

    Create AvatarThumbnail.cs

---
+++ NextGenSoftware.OASIS.API.Core/Holons/AvatarThumbnail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NextGenSoftware.OASIS.API.Core.Interfaces;
+
+namespace NextGenSoftware.OASIS.API.Core.Holons
+{
+    public class AvatarThumbnail : IAvatarThumbnail
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+    }
+}

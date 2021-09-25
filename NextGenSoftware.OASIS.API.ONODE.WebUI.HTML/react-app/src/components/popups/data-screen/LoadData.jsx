d3b456819014b2287e378147c2eafeae0c263090
Muhammad Arslan <m.arslan2037@gmail.com>
2021-09-26 03:35:55 +0500

    ag grid is added in load data popup

--- NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/components/popups/data-screen/LoadData.jsx
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/components/popups/data-screen/LoadData.jsx
@@ -1,60 +1,45 @@
 import React from 'react';
 
 import { Modal } from 'react-bootstrap';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
-import '../../../assets/scss/DataScreen.scss';
+import '../../../assets/scss/data-screen.scss';
 
 class LoadData extends React.Component {
+
+    rowData = [
+        {make: "Toyota", model: "Celica", price: 35000},
+        {make: "Ford", model: "Mondeo", price: 32000},
+        {make: "Porsche", model: "Boxter", price: 72000}
+    ];
+
     render() { 
         const { show, hide } = this.props;
 
         return (
             <>
-                <Modal centered className="custom-modal custom-popup-component" show={show} onHide={() => hide('loadData')}>
+                <Modal 
+                    centered 
+                    className="custom-modal custom-popup-component light-custom-popup" 
+                    show={show}
+                    dialogClassName="modal-90w"
+                    onHide={() => hide('loadData')}
+                >
                     <Modal.Body>
                         <span className="form-cross-icon" onClick={() => hide('loadData')}>
                             <i className="fa fa-times"></i>
                         </span>
 
-                        <div className="popup-container default-popup">
-                            <div className="data-screen-container">
-                                <h2>Data</h2>
-
-                                <div className="form-container">
-                                    <form>
-                                        <p className="single-form-row">
-                                            <label>Provider: </label>
-                                            <span className="have-selectbox">
-                                                <select className="custom-selectbox">
-                                                    <option>EOSIS</option>
-                                                    <option>EOSIS - 1</option>
-                                                    <option>EOSIS - 2</option>
-                                                </select>
-                                                <i className="fa fa-angle-down"></i>
-                                            </span>
-
-                                            <span className="have-icon"></span>
-                                        </p>
-
-                                        <p className="single-form-row">
-                                            <label>Json: </label>
-                                            <textarea></textarea>
-                                        </p>
-
-                                        <p className="single-form-row">
-                                            <label>File: </label>
-                                            <input type="file" />
-                                        </p>
-
-                                        <p className="single-form-row">
-                                            <button 
-                                                className="submit-button" 
-                                                type="submit"
-                                            >Save</button>
-                                        </p>
-                                    </form>
-                                </div>
-                            </div>
+                        <div className="ag-theme-alpine custom-ag-parent">
+                            <AgGridReact
+                                rowData={this.rowData}>
+                                <AgGridColumn field="date"></AgGridColumn>
+                                <AgGridColumn field="size"></AgGridColumn>
+                                <AgGridColumn field="file"></AgGridColumn>
+                                <AgGridColumn field="edit"></AgGridColumn>
+                                <AgGridColumn field="view"></AgGridColumn>
+                                <AgGridColumn field="delete"></AgGridColumn>
+                            </AgGridReact>
                         </div>
                     </Modal.Body>
                 </Modal>

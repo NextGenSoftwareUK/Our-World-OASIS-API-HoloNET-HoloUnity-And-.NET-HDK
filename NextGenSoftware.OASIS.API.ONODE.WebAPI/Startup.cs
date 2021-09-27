74e48f9accf1b0754a581288e62707cca98b4cb7
David Ellams <davidellams@hotmail.com>
2021-09-27 22:05:28 +0100

    - Added v0.17.1 Release notes to OASIS API RELEASE HISTORY.
    - Added v0.17.2 Release notes to OASIS API RELEASE HISTORY.
    
    - Bumped OASIS API REST WEB API to v0.17.2 ALPHA.

--- NextGenSoftware.OASIS.API.ONODE.WebAPI/Startup.cs
+++ NextGenSoftware.OASIS.API.ONODE.WebAPI/Startup.cs
@@ -18,7 +18,7 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI
 {
     public class Startup
     {
-        private const string VERSION = "OASIS API v0.17 ALPHA";
+        private const string VERSION = "OASIS API v0.17.2 ALPHA";
         readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         public Startup(IConfiguration configuration)

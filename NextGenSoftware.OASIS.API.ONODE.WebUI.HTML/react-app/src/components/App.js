19de36bbafa0285c9cecadef4ba2593ee25c32d9
waqarmm <wiki.mir@gmail.com>
2021-09-28 22:14:36 +0500

    changes by waqar

--- NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/components/App.js
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/components/App.js
@@ -14,7 +14,10 @@ import AcceptInvite from "./pages/seeds/AcceptInvite";
 import AddData from "./popups/data-screen/AddData";
 import LoadData from "./popups/data-screen/LoadData";
 
-import ViewAvatar from "./pages/avatar/viewAvatar"
+import ViewAvatar from "./pages/avatar/viewAvatar";
+import UploadAvatar from "../components/pages/avatar/uploadAvatar";
+import Provider from "../components/pages/providers/Provider";
+import Keymanagement from "../components/pages/providers/KeyManagement";
 
 import "../assets/scss/general.scss";
 import "../assets/scss/style.scss";
@@ -23,168 +26,177 @@ import "../assets/scss/seeds.scss";
 import axios from "axios";
 
 class App extends React.Component {
-    state = {
-        showSidebar: false,
-        showLogin: false,
-        showSignup: false,
-        user: null,
+  state = {
+    showSidebar: false,
+    showLogin: false,
+    showSignup: false,
+    user: null,
 
-        dataScreen: {
-            loadData: false,
-            sendData: false,
-            manageData: false
-        }
+    dataScreen: {
+      loadData: false,
+      sendData: false,
+      manageData: false,
+    },
+  };
+
+  componentDidMount() {
+    localStorage.getItem("user");
+
+    if (localStorage.getItem("user")) {
+      this.setState({ user: JSON.parse(localStorage.getItem("user")) });
     }
+  }
 
-    componentDidMount() {
-        localStorage.getItem('user')
+  setUserData = (data) => {
+    console.log(data);
+    this.setState({
+      user: data,
+    });
+  };
 
-        if (localStorage.getItem('user')) {
-            this.setState({ user: JSON.parse(localStorage.getItem('user')) })
-        }
-    }
+  toggleSidebar = () => {
+    this.setState({
+      showSidebar: !this.state.showSidebar,
+    });
+  };
 
-    setUserData = (data) => {
-        console.log(data);
-        this.setState({
-            user: data
-        })
-    }
+  hidePopups = () => {
+    this.setState({
+      showLogin: false,
+      showSignup: false,
+    });
+  };
 
-    toggleSidebar = () => {
-        this.setState({
-            showSidebar: !this.state.showSidebar
-        })
-    }
+  hideLogin = () => {
+    this.setState({
+      showLogin: false,
+    });
+  };
 
-    hidePopups = () => {
-        this.setState({
-            showLogin: false,
-            showSignup: false
-        })
-    }
+  hideSignup = () => {
+    this.setState({
+      showSignup: false,
+    });
+  };
 
-    hideLogin = () => {
-        this.setState({
-            showLogin: false,
-        })
-    }
+  showLogin = () => {
+    this.setState({
+      showLogin: true,
+      showSignup: false,
+    });
+  };
 
-    hideSignup = () => {
-        this.setState({
-            showSignup: false,
-        })
-    }
+  showSignup = () => {
+    this.setState({
+      showSignup: true,
+      showLogin: false,
+    });
+  };
 
-    showLogin = () => {
-        this.setState({
-            showLogin: true,
-            showSignup: false,
-        })
-    }
+  handleLogout = () => {
+    axios
+      .post("https://api.oasisplatform.world/api/avatar/revoke-token", {
+        token: this.state.user.jwtToken,
+      })
+      .then((res) => {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+        localStorage.removeItem("credentials");
+      })
+      .catch((err) => {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+        localStorage.removeItem("credentials");
+      });
+  };
 
-    showSignup = () => {
-        this.setState({
-            showSignup: true,
-            showLogin: false
-        })
-    }
+  toggleDataScreenPopup = (name) => {
+    let dataScreen = { ...this.state.dataScreen };
+    dataScreen[name] = !dataScreen[name];
 
-    handleLogout = () => {
-        axios.post('https://api.oasisplatform.world/api/avatar/revoke-token', {
-            token: this.state.user.jwtToken
-        }).then(res => {
-            this.setState({ user: null })
-            localStorage.removeItem('user')
-            localStorage.removeItem('credentials')
-        }).catch(err => {
-            this.setState({ user: null })
-            localStorage.removeItem('user')
-            localStorage.removeItem('credentials')
-        })
-    }
+    this.setState({
+      dataScreen,
+    });
+  };
 
-    toggleDataScreenPopup = (name) => {
-        let dataScreen = {...this.state.dataScreen};
-        dataScreen[name] = !dataScreen[name];
-
-        this.setState({
-            dataScreen
-        })
-    }
-
-    render() {
-        return (
-            <div className="main-container">
-                <Router>
-                    <header>
-                        <Navbar
-                            showSidebar={this.state.showSidebar}
-                            toggleSidebar={this.toggleSidebar}
-                            showLogin={this.showLogin}
-                            showSignup={this.showSignup}
-                            handleLogout={this.handleLogout}
-                            user={this.state.user}
-                        />
-                        {/* <SideNav
+  render() {
+    return (
+      <div className="main-container">
+        <Router>
+          <header>
+            <Navbar
+              showSidebar={this.state.showSidebar}
+              toggleSidebar={this.toggleSidebar}
+              showLogin={this.showLogin}
+              showSignup={this.showSignup}
+              handleLogout={this.handleLogout}
+              user={this.state.user}
+            />
+            {/* <SideNav
                             showSidebar={this.state.showSidebar}
                             toggleSidebar={this.toggleSidebar}
                         /> */}
-                        <Sidebar 
-                            showSidebar={this.state.showSidebar} 
-                            toggleSidebar={this.toggleSidebar} 
-                            toggleDataScreenPopup={this.toggleDataScreenPopup}
-                        />
-                    </header>
+            <Sidebar
+              showSidebar={this.state.showSidebar}
+              toggleSidebar={this.toggleSidebar}
+              toggleDataScreenPopup={this.toggleDataScreenPopup}
+            />
+          </header>
 
-                    <div className="content-container">
-                        <Switch>
-                            <Route exact path="/home" component={Home} />
+          <div className="content-container">
+            <Switch>
+              <Route exact path="/home" component={Home} />
 
-                            <Route path="/pay-with-seeds" component={PayWithSeeds} />
-                            <Route path="/donateWithSeeds">
-                                <PayWithSeeds seedType="Donate" />
-                            </Route>
-                            <Route path="/rewardWithSeeds">
-                                <PayWithSeeds seedType="Reward" />
-                            </Route>
-                            <Route path="/accept-invite-to-join-seeds" component={AcceptInvite} />
-                            <Route path="/send-invite" component={SendInvite} />
-                            <Route exact path="/karma" component={Karma} />
-                            <Route exact path="/avatar/view" component={ViewAvatar} />
-                        </Switch>
-                    </div>
-                </Router>
+              <Route path="/pay-with-seeds" component={PayWithSeeds} />
+              <Route path="/donateWithSeeds">
+                <PayWithSeeds seedType="Donate" />
+              </Route>
+              <Route path="/rewardWithSeeds">
+                <PayWithSeeds seedType="Reward" />
+              </Route>
+              <Route
+                path="/accept-invite-to-join-seeds"
+                component={AcceptInvite}
+              />
+              <Route path="/send-invite" component={SendInvite} />
+              <Route exact path="/karma" component={Karma} />
+              <Route exact path="/avatar/view" component={ViewAvatar} />
+              <Route exact path="/avatar/upload" component={UploadAvatar} />
+              <Route path="/provider/provider" component={Provider} />
+              <Route path="/provider/keymanagement" component={Keymanagement} />
+            </Switch>
+          </div>
+        </Router>
 
-                <Login
-                    className="custom-form"
-                    show={this.state.showLogin}
-                    hide={this.hideLogin}
-                    change={this.showSignup}
-                    setUserStateData={this.setUserData}
-                />
+        <Login
+          className="custom-form"
+          show={this.state.showLogin}
+          hide={this.hideLogin}
+          change={this.showSignup}
+          setUserStateData={this.setUserData}
+        />
 
-                <Signup
-                    className="custom-form"
-                    show={this.state.showSignup}
-                    hide={this.hideSignup}
-                    change={this.showLogin}
-                />
+        <Signup
+          className="custom-form"
+          show={this.state.showSignup}
+          hide={this.hideSignup}
+          change={this.showLogin}
+        />
 
-                {/* ========== DATA SCREEN POPUPS START ========== */}
-                <AddData 
-                    show={this.state.dataScreen.sendData}
-                    hide={this.toggleDataScreenPopup}
-                />
+        {/* ========== DATA SCREEN POPUPS START ========== */}
+        <AddData
+          show={this.state.dataScreen.sendData}
+          hide={this.toggleDataScreenPopup}
+        />
 
-                <LoadData 
-                    show={this.state.dataScreen.loadData}
-                    hide={this.toggleDataScreenPopup}
-                />
-                {/* ========== DATA SCREEN POPUPS START ========== */}
-            </div>
-        );
-    }
+        <LoadData
+          show={this.state.dataScreen.loadData}
+          hide={this.toggleDataScreenPopup}
+        />
+        {/* ========== DATA SCREEN POPUPS START ========== */}
+      </div>
+    );
+  }
 }
 
 export default App;

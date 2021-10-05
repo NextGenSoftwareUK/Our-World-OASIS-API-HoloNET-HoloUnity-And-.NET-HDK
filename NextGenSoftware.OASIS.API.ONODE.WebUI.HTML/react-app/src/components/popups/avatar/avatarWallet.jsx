b9042b1647db8e4c4cfb9ac99e26c9beaf72cee5
derek-dv <derekmiracledavid@hotmail.com>
2021-10-05 11:12:38 +0100

    avatar wallet component

---
+++ NextGenSoftware.OASIS.API.ONODE.WebUI.HTML/react-app/src/components/popups/avatar/avatarWallet.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+class AvatarWallet extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			columns: [],
+			rows: [],
+			loading: true,
+			loggedIn: true,
+		}
+	}
+
+	componentDidMount(){
+		// Api calls
+		return
+	}
+
+	render() {
+		<div className="popup-container">
+			avatarWallet
+		</div>
+	}
+}
+
+export default AvatarWallet

8fdb75a2dd516b108a6465beadc9e11eea692cde
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-07 20:04:36 +0500

    Update SolanaController.cs

--- NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/SolanaController.cs
+++ NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/SolanaController.cs
@@ -9,7 +9,7 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class SolanaController : OASISControllerBase
     {
         private readonly ISolanaService _solanaService;
@@ -23,8 +23,7 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         [Route("Mint")]
         public async Task<OASISResult<MintNftResult>> MintNft([FromBody] MintNftRequest request)
         {
-            var mintNftResult = await _solanaService.MintNft(request);
-            return new OASISResult<MintNftResult>(mintNftResult.Payload);
+            return await _solanaService.MintNft(request);
         }
 
         
@@ -32,32 +31,28 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         [Route("Exchange")]
         public async Task<OASISResult<ExchangeTokenResult>> ExchangeToken([FromBody] ExchangeTokenRequest request)
         {
-            var exchangeTokens = await _solanaService.ExchangeTokens(request);
-            return new OASISResult<ExchangeTokenResult>(exchangeTokens.Payload);
+            return await _solanaService.ExchangeTokens(request);
         }
 
         [HttpPost]
         [Route("Send")]
         public async Task<OASISResult<SendTransactionResult>> SendTransaction([FromBody] SendTransactionRequest request)
         {
-            var transactResult = await _solanaService.SendTransaction(request);
-            return new OASISResult<SendTransactionResult>(transactResult.Payload);
+            return await _solanaService.SendTransaction(request);
         }
 
         [HttpGet]
         [Route("GetNftMetadata")]
         public async Task<OASISResult<GetNftMetadataResult>> GetNftMetadata(GetNftMetadataRequest request)
         {
-            var metadata = await _solanaService.GetNftMetadata(request);
-            return new OASISResult<GetNftMetadataResult>(metadata.Payload);
+            return await _solanaService.GetNftMetadata(request);
         }
 
         [HttpGet]
         [Route("GetNftWallet")]
         public async Task<OASISResult<GetNftWalletResult>> GetNftWallet(GetNftWalletRequest request)
         {
-            var wallet = await _solanaService.GetNftWallet(request);
-            return new OASISResult<GetNftWalletResult>(wallet.Payload);
+            return await _solanaService.GetNftWallet(request);
         }
     }
 }

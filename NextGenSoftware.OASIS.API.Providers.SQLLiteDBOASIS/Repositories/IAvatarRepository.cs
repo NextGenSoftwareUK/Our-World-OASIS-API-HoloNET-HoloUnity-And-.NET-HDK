310c867f6664494c33c8a41c1f1d0d026cba2b22
neurip <neuripa@gmail.com>
2021-10-09 16:51:08 -0400

    Fixes for asyn methods, and all return OASISResult

--- NextGenSoftware.OASIS.API.Providers.SQLLiteDBOASIS/Repositories/IAvatarRepository.cs
+++ NextGenSoftware.OASIS.API.Providers.SQLLiteDBOASIS/Repositories/IAvatarRepository.cs
@@ -1,42 +1,44 @@
 using System;
 using System.Collections.Generic;
 using NextGenSoftware.OASIS.API.Core.Holons;
+using NextGenSoftware.OASIS.API.Core.Helpers;
 using System.Threading.Tasks;
 
 namespace NextGenSoftware.OASIS.API.Providers.SQLLiteDBOASIS.Repositories{
 
     public interface IAvatarRepository
     {
-        Avatar Add(Avatar avatar);
-        Task<Avatar> AddAsync(Avatar avatar);
-        AvatarDetail Add(AvatarDetail avatar);
-        Task<AvatarDetail> AddAsync(AvatarDetail avatar);
-        bool Delete(Guid id, bool softDelete = true);
-        bool Delete(string userName, bool softDelete = true);
-        bool DeleteByEmail(string avatarEmail, bool softDelete = true);
-        Task<bool> DeleteAsync(Guid id, bool softDelete = true);
-        Task<bool> DeleteAsync(string userName, bool softDelete = true);
-        Task<bool> DeleteByEmailAsync(string avatarEmail, bool softDelete = true);
-        Avatar GetAvatar(Guid id);
-        Avatar GetAvatar(string username);
-        Avatar GetAvatar(string username, string password);
-        Task<Avatar> GetAvatarAsync(Guid id);
-        Task<Avatar> GetAvatarAsync(string username);
-        Task<Avatar> GetAvatarByEmailAsync(string avatarEmail);
-        Task<Avatar> GetAvatarAsync(string username, string password);
-        List<Avatar> GetAvatars();
-        Task<List<Avatar>> GetAvatarsAsync();
-        AvatarDetail GetAvatarDetail(Guid id);
-        AvatarDetail GetAvatarDetail(string username);
-        AvatarDetail GetAvatarDetailByEmail(string avatarEmail);
-        Task<AvatarDetail> GetAvatarDetailAsync(Guid id);
-        Task<AvatarDetail> GetAvatarDetailAsync(string username);
-        Task<AvatarDetail> GetAvatarDetailByEmailAsync(string avatarEmail);
-        Task<IEnumerable<AvatarDetail>> GetAvatarDetailsAsync();
-        IEnumerable<AvatarDetail> GetAvatarDetails();
-        Avatar Update(Avatar avatar);
-        Task<Avatar> UpdateAsync(Avatar avatar);
-        AvatarDetail Update(AvatarDetail avatar);
-        Task<AvatarDetail> UpdateAsync(AvatarDetail avatar);
+        OASISResult<Avatar> Add(Avatar avatar);
+        Task<OASISResult<Avatar>> AddAsync(Avatar avatar);
+        OASISResult<AvatarDetail> Add(AvatarDetail avatar);
+        Task<OASISResult<AvatarDetail>> AddAsync(AvatarDetail avatar);
+        OASISResult<bool> Delete(Guid id, bool softDelete = true);
+        OASISResult<bool> Delete(string userName, bool softDelete = true);
+        OASISResult<bool> DeleteByEmail(string avatarEmail, bool softDelete = true);
+        Task<OASISResult<bool>> DeleteAsync(Guid id, bool softDelete = true);
+        Task<OASISResult<bool>> DeleteAsync(string userName, bool softDelete = true);
+        Task<OASISResult<bool>> DeleteByEmailAsync(string avatarEmail, bool softDelete = true);
+        OASISResult<Avatar> GetAvatar(Guid id);
+        OASISResult<Avatar> GetAvatar(string username);
+        OASISResult<Avatar> GetAvatar(string username, string password);
+        Task<OASISResult<Avatar>> GetAvatarAsync(Guid id);
+        Task<OASISResult<Avatar>> GetAvatarAsync(string username);
+        OASISResult<Avatar> GetAvatarByEmail(string avatarEmail);
+        Task<OASISResult<Avatar>> GetAvatarByEmailAsync(string avatarEmail);
+        Task<OASISResult<Avatar>> GetAvatarAsync(string username, string password);
+        OASISResult<List<Avatar>> GetAvatars();
+        Task<OASISResult<List<Avatar>>> GetAvatarsAsync();
+        OASISResult<AvatarDetail> GetAvatarDetail(Guid id);
+        OASISResult<AvatarDetail> GetAvatarDetail(string username);
+        OASISResult<AvatarDetail> GetAvatarDetailByEmail(string avatarEmail);
+        Task<OASISResult<AvatarDetail>> GetAvatarDetailAsync(Guid id);
+        Task<OASISResult<AvatarDetail>> GetAvatarDetailAsync(string username);
+        Task<OASISResult<AvatarDetail>> GetAvatarDetailByEmailAsync(string avatarEmail);
+        Task<OASISResult<IEnumerable<AvatarDetail>>> GetAvatarDetailsAsync();
+        OASISResult<IEnumerable<AvatarDetail>> GetAvatarDetails();
+        OASISResult<Avatar> Update(Avatar avatar);
+        Task<OASISResult<Avatar>> UpdateAsync(Avatar avatar);
+        OASISResult<AvatarDetail> Update(AvatarDetail avatar);
+        Task<OASISResult<AvatarDetail>> UpdateAsync(AvatarDetail avatar);
     }
 }

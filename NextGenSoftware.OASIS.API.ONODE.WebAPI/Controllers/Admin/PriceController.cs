d90859e90c97da5a12dbb57b6d1bf12828267bdf
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-10 23:17:18 +0500

    added routes

--- NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/Admin/PriceController.cs
+++ NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/Admin/PriceController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NextGenSoftware.OASIS.API.Core.Helpers;
 
 namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers.Admin
 {
@@ -7,6 +10,37 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers.Admin
     [Authorize]
     public class PriceController : OASISControllerBase
     {
-        
+        [HttpPost]
+        public async Task<OASISResult<bool>> Create()
+        {
+            
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<OASISResult<bool>> Update(int id)
+        {
+            
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<OASISResult<bool>> Delete(int id)
+        {
+            
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<OASISResult<OlandPriceDto>> Get(int id)
+        {
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<OASISResult<IEnumerable<OlandPriceDto>>> GetAll()
+        {
+            
+        }
+    }
+
+    public sealed class OlandPriceDto
+    {
     }
 }

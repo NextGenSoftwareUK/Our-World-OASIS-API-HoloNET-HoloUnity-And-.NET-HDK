c6df05365f40fb9c4c454c7e2f4e1f9fc99a69be
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-10 23:03:47 +0500

    added price controller

---
+++ NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/Admin/PriceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers.Admin
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PriceController : OASISControllerBase
+    {
+        
+    }
+}

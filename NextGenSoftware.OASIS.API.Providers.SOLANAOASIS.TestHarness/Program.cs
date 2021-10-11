49ba00568347c076e0b278575e315b18258849fe
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-11 23:18:50 +0500

    harness project for testing crearted

---
+++ NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.TestHarness/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Solnet.Programs.Utilities;
+
+namespace NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.TestHarness
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var publicKey = "GsguXojeGATpZGW8VNfW8qQCBVodbW2qGS8bUEbdGZfv";
+            
+            byte[] bytes = Encoding.ASCII.GetBytes(publicKey);
+            ReadOnlySpan<byte> binData = new(bytes);
+            Console.WriteLine(binData.Length);
+            Console.WriteLine(binData.IsEmpty);
+
+            Console.WriteLine(binData.GetU32(1));
+            Console.WriteLine(binData.GetS32(3));
+        }
+    }
+}

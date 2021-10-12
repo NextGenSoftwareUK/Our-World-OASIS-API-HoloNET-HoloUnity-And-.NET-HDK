590597391d8418fecb175b53c1542dcb1d1513be
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-13 02:23:26 +0500

    Create Oland.cs

---
+++ NextGenSoftware.OASIS.API.Providers.MongoOASIS/Entities/Oland.cs
@@ -0,0 +1,13 @@
+﻿namespace NextGenSoftware.OASIS.API.Providers.MongoDBOASIS.Entities
+{
+    public class Oland
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int OlandsCount { get; set; }
+        public decimal TopSize { get; set; }
+        public decimal RightSize { get; set; }
+        public string UnitOfMeasure { get; set; }
+    }
+}

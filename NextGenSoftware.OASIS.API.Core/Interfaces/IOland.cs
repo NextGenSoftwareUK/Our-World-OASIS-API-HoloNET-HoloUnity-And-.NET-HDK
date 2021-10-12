f5b49c5010a4b1e00c88520d5e9a94986d2dc8e6
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-13 01:49:58 +0500

    Create IOland.cs

---
+++ NextGenSoftware.OASIS.API.Core/Interfaces/IOland.cs
@@ -0,0 +1,15 @@
+﻿namespace NextGenSoftware.OASIS.API.Core.Interfaces
+{
+    public interface IOland
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int OlandsCount { get; set; }
+        public decimal TopSize { get; set; }
+        public decimal RightSize { get; set; }
+        public string UnitOfMeasure  { get; set; }
+
+        string UnitSize => $"{TopSize}x{RightSize} {UnitOfMeasure}";
+    }
+}

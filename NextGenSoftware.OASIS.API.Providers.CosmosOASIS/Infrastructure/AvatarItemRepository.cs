d8412af8f5866f8cee8bc2d47309786f317478ca
Muhammad Faizan <Faizan_shaikh_619@hotmail.com>
2021-10-12 16:56:13 +0500

    - Added Infrastructure and Repository of Cosmos DB

---
+++ NextGenSoftware.OASIS.API.Providers.CosmosOASIS/Infrastructure/AvatarItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Documents;
+using NextGenSoftware.OASIS.API.Providers.CosmosOASIS.Entites;
+using NextGenSoftware.OASIS.API.Providers.CosmosOASIS.Interfaces;
+
+namespace NextGenSoftware.OASIS.API.Providers.CosmosOASIS.Infrastructure
+{
+    public class AvatarItemRepository : CosmosDbRepository<Avatar> , IAvatarItemRepository
+    {
+        public AvatarItemRepository(ICosmosDbClientFactory factory) : base(factory) { }
+
+        public override string CollectionName { get; } = "avatarItems";
+        public override string GenerateId(Avatar entity) => $"{entity.AvatarType.Name}:{Guid.NewGuid()}";
+        public override PartitionKey ResolvePartitionKey(string entityId) => new PartitionKey(entityId.Split(':')[0]);
+    }
+}
---
+++ NextGenSoftware.OASIS.API.Providers.CosmosOASIS/Infrastructure/IDocumentCollectionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Documents;
+using NextGenSoftware.OASIS.API.Providers.CosmosOASIS.Entites;
+
+namespace NextGenSoftware.OASIS.API.Providers.CosmosOASIS.Infrastructure
+{
+    public interface IDocumentCollectionContext<in T> where T : Entity
+    {
+        string CollectionName { get; }
+
+        string GenerateId(T entity);
+
+        PartitionKey ResolvePartitionKey(string entityId);
+    }
+}

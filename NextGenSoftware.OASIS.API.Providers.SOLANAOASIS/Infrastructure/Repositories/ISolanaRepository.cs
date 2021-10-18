01ddf7bbd6a00ea2c70cb9c4bfc446a1af6ae064
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-18 08:04:46 +0500

    solana repository created

---
+++ NextGenSoftware.OASIS.API.Providers.SOLANAOASIS/Infrastructure/Repositories/ISolanaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Core.Interfaces;
+
+namespace NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.Infrastructure.Repositories
+{
+    public interface ISolanaRepository<T>
+    {
+        Task<string> CreateAsync(T entity);
+        Task<string> UpdateAsync(T avatar, string hash);
+        Task<string> DeleteAsync(string hash);
+        Task<IAvatar> GetAsync(string hash);
+        
+        Task<string> Create(T entity);
+        Task<string> Update(T avatar, string hash);
+        Task<string> Delete(string hash);
+        Task<IAvatar> Get(string hash);
+    }
+}
---
+++ NextGenSoftware.OASIS.API.Providers.SOLANAOASIS/Infrastructure/Repositories/SolanaRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Core.Interfaces;
+using Solnet.Rpc;
+
+namespace NextGenSoftware.OASIS.API.Providers.SOLANAOASIS.Infrastructure.Repositories
+{
+    public class SolanaRepository<T> : ISolanaRepository<T>
+    {
+        private IRpcClient _rpcClient;
+
+        public SolanaRepository()
+        {
+            _rpcClient = ClientFactory.GetClient(Cluster.MainNet);
+        }
+
+        public async Task<string> CreateAsync(T entity)
+        {
+        }
+
+        public async Task<string> UpdateAsync(T avatar, string hash)
+        {
+        }
+
+        public async Task<string> DeleteAsync(string hash)
+        {
+        }
+
+        public async Task<IAvatar> GetAsync(string hash)
+        {
+        }
+
+        public async Task<string> Create(T entity)
+        {
+        }
+
+        public async Task<string> Update(T avatar, string hash)
+        {
+        }
+
+        public async Task<string> Delete(string hash)
+        {
+        }
+
+        public async Task<IAvatar> Get(string hash)
+        {
+        }
+    }
+}

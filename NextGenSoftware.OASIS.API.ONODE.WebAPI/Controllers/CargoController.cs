09d5d73a0608a96332bbb5582550d97c5348fd86
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-10-22 11:40:40 +0500

    cargo api secified

--- NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/CargoController.cs
+++ NextGenSoftware.OASIS.API.ONODE.WebAPI/Controllers/CargoController.cs
@@ -26,10 +26,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Authorize Account with optional (email and username) or required account address
         /// </summary>
-        /// <param name="requestModel"></param>
-        /// <returns></returns>
+        /// <param name="requestModel">Account Address that need to be authorized</param>
+        /// <returns>JWT Token</returns>
         [HttpPost]
         [Route("AuthorizeAccount")]
         public async Task<OASISResult<CreateAccountResponseModel>> AuthorizeAccount(
@@ -39,10 +39,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Authenticate specified account address 
         /// </summary>
-        /// <param name="requestModel"></param>
-        /// <returns></returns>
+        /// <param name="requestModel">Account Address</param>
+        /// <returns>JWT Token</returns>
         [HttpPost]
         [Route("AuthenticateAccount")]
         public async Task<OASISResult<CreateAccountResponseModel>> AuthenticateAccount([FromBody] AuthenticateAccountRequestModel requestModel)
@@ -51,10 +51,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Handling cargo purchasing with specified saleId
         /// </summary>
-        /// <param name="requestModel"></param>
-        /// <returns></returns>
+        /// <param name="requestModel">SaleId that need to be purchased</param>
+        /// <returns>Purchase Transaction Hash</returns>
         [HttpPost]
         [Route("PurchaseSale")]
         public async Task<OASISResult<PurchaseResponseModel>> PurchaseCargoSale([FromBody] PurchaseRequestModel requestModel)
@@ -63,10 +63,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Handling cargo sale cancellation
         /// </summary>
-        /// <param name="requestModel"></param>
-        /// <returns></returns>
+        /// <param name="requestModel">The ID of the resale item</param>
+        /// <returns>Cancellation signature</returns>
         [HttpPost]
         [Route("CancelSale")]
         public async Task<OASISResult<CancelSaleResponseModel>> CancelCargoSale([FromBody] CancelSaleRequestModel requestModel)
@@ -75,10 +75,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Returning orders list
         /// </summary>
-        /// <param name="orderParams"></param>
-        /// <returns></returns>
+        /// <param name="orderParams">Fetching order list request</param>
+        /// <returns>Orders List</returns>
         [HttpGet]
         [Route("GetOrders")]
         public async Task<OASISResult<PaginationResponseWithResults<IEnumerable<Order>>>> GetCargoOrders(
@@ -88,10 +88,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Returns user token list by specified contract
         /// </summary>
-        /// <param name="requestModel"></param>
-        /// <returns></returns>
+        /// <param name="requestModel">Fetching user token list request</param>
+        /// <returns>User token with specified contract</returns>
         [HttpGet]
         [Route("GetUserTokensByContract")]
         public async Task<OASISResult<GetUserTokensByContractResponseModel>> GetUserTokensByContract(
@@ -101,10 +101,10 @@ namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Returns collectibles list by specified project id
         /// </summary>
-        /// <param name="requestModel"></param>
-        /// <returns></returns>
+        /// <param name="requestModel">Project Id</param>
+        /// <returns>Collectibles list by specified project id</returns>
         [HttpGet]
         [Route("GetCollectiblesListByProjectId")]
         public async Task<OASISResult<GetCollectiblesListByProjectIdResponseModel>> CollectiblesListByProjectId(

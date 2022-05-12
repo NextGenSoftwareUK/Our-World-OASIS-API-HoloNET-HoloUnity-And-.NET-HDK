8bd345144e70758596c155bee64876dc69781179
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-05-13 01:49:22 +0500

    initial eos commit, added eos tets harness project

---
+++ NextGenSoftware.OASIS.API.Providers.EOSIOOASIS.TestHarness/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Core.Holons;
+
+namespace NextGenSoftware.OASIS.API.Providers.EOSIOOASIS.TestHarness
+{
+    internal static class Program
+    {
+        public static async Task Main()
+        {
+            EOSIOOASIS eosProvider = new EOSIOOASIS("https://api.eosnewyork.io");
+            eosProvider.ActivateProvider();
+            await eosProvider.SaveAvatarAsync(new Avatar()
+            {
+                FirstName = "Pop",
+                LastName = "Nod",
+                Id = Guid.NewGuid(),
+                AvatarId = Guid.NewGuid(),
+                Password = "Oops..."
+            });
+        }
+    }
+}

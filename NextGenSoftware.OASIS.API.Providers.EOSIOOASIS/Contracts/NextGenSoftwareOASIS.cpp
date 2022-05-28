0309d453978903e79c493e3a937046c4a731dfc9
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-05-28 19:18:38 +0500

    eos smart contract cpp rewrited

--- NextGenSoftware.OASIS.API.Providers.EOSIOOASIS/Contracts/NextGenSoftwareOASIS.cpp
+++ NextGenSoftware.OASIS.API.Providers.EOSIOOASIS/Contracts/NextGenSoftwareOASIS.cpp
@@ -11,259 +11,202 @@
 
 // Inserts an avatar entity into avatars list
 [[eosio::action]]
-void NextGenSoftwareOASIS::addavatar(long entityId, string avatarId, string info) 
+void NextGenSoftwareOASIS::addavatar(long entityId, std::string avatarId, std::string info) 
 {
-    // Validating step: check if avatar with current id is exist
-    for (auto avatarItem : avatarTable) 
-    {
-        if(avatarItem.entityId == entityId)
-            return;
-    }
+    eosio::print("Avatar creating started...");
 
-    // Insert avatar entity into avatar table
-    avatar avatarEntity;
-    avatarEntity.entityId = entityId;
-    avatarEntity.isDeleted = false;
-    avatarEntity.avatarId = avatarId;
-    avatarEntity.info = info;
-
-    avatarTable.push_back(avatarEntity);
+    // Insert new avatar
+    _avtrs.emplace(get_self(), [&](auto& a) {
+        a.key = _dtls.available_primary_key();
+        a.entityId = entityId;
+        a.avatarId = avatarId;
+        a.info = info;
+        a.isDeleted = false;
+    });
 
     // Print insertation result
-    print("New avatar created, ID: ", entityId);
-}
-
-// Reads an avatar entity from avatars list by its entity id
-[[eosio::action]]
-NextGenSoftwareOASIS::avatar NextGenSoftwareOASIS::getavatar(long entityId) 
-{
-    for (auto avatarItem : avatarTable) 
-        if(avatarItem.entityId == entityId && avatarItem.isDeleted == false) 
-        {
-            // Print reading result
-            print("Reading avatar executed, readed avatar with ID: ", avatarItem.entityId);
-
-            return avatarItem;
-        }   
-
-    print("Reading avatar executed, avatar not found with ID: ", entityId);
-
-    // if nothing was found
-    return avatar{};
-}
-
-// Reads all avatars from avatars list
-[[eosio::action]]
-NextGenSoftwareOASIS::avatar* NextGenSoftwareOASIS::getavatars() 
-{ 
-    return &avatarTable[0];
+    eosio::print("New avatar created, ID: ", entityId);
 }
 
 // Updates avatar info field by specified avatar entity id
 [[eosio::action]]
-void NextGenSoftwareOASIS::setavatar(long entityId, string info) 
+void NextGenSoftwareOASIS::setavatar(long entityId, std::string info) 
 {
-    for (auto avatarItem : avatarTable) 
-        if(avatarItem.entityId == entityId && avatarItem.isDeleted == false) 
-        {
-            avatarItem.info = info;
+    eosio::print("Avatar updating started...");
 
-            // Print reading result
-            print("Updating completed, Avatar ID: ", avatarItem.entityId);
-            return;
-        }  
+    std::vector<uint64_t> keysForModify;
+    for(auto& item : _avtrs) {
+        if (item.entityId == entityId) {
+            keysForModify.push_back(item.key);   
+        }
+    }
+    
+    for (uint64_t key : keysForModify) {
+        auto itr = _avtrs.find(key);
+        if (itr != _avtrs.end()) {
+            _avtrs.modify(itr, get_self(), [&](auto& a) {
+                a.info = info;
+            });
+        }
+    }
     
     // Print updating result
-    print("Avatar not found, updating failed, ID: ", entityId);
+    eosio::print("Avatar updated, ID: ", entityId);
 }
 
 // Removes avatar entity specified by its entity id from avatars list
 [[eosio::action]]
 void NextGenSoftwareOASIS::hardavatar(long entityId) 
 {
-    for (auto avatarItem : avatarTable) 
-        if(avatarItem.entityId == entityId && avatarItem.isDeleted == false) 
-        {
-            // TODO: remove avatar from the list
-            // avatarTable.erase(remove(avatarTable.begin(), avatarTable.end(), avatarItem), avatarTable.end());
-         
-            // Print reading result
-            print("Avatar deleted, ID: ", entityId);
-            return;
-        }  
+    std::vector<uint64_t> keysForDeletion;
 
+    for(auto& item : _avtrs) {
+        if (item.entityId == entityId) {
+            keysForDeletion.push_back(item.key);   
+        }
+    }
+    
+    for (uint64_t key : keysForDeletion) {
+        auto itr = _avtrs.find(key);
+        if (itr != _avtrs.end()) {
+            _avtrs.erase(itr);
+        }
+    }
+    
     // Print hard-deleting result
-    print("Avatar not found, deleting failed, ID: ", entityId);
+    eosio::print("Avatar hard deleted, ID: ", entityId);
 }
 
 // Sets avatar IsDeleted field value to true specified by entity id
 [[eosio::action]]
 void NextGenSoftwareOASIS::softavatar(long entityId) 
 {
-    for (auto avatarItem : avatarTable) 
-        if(avatarItem.entityId == entityId && avatarItem.isDeleted == false) 
-        {
-            avatarItem.isDeleted = true;
-
-            // Print reading result
-            print("Avatar soft delete failed, ID: ", avatarItem.entityId);
-            return;
-        }  
+    std::vector<uint64_t> keysForModify;
+    for(auto& item : _avtrs) {
+        if (item.entityId == entityId) {
+            keysForModify.push_back(item.key);   
+        }
+    }
+    
+    for (uint64_t key : keysForModify) {
+        auto itr = _avtrs.find(key);
+        if (itr != _avtrs.end()) {
+            _avtrs.modify(itr, get_self(), [&](auto& a) {
+                a.isDeleted = true;
+            });
+        }
+    }
 
     // Print soft-deleting result
-    print("Avatar not found, soft deleted failed, ID: ", entityId);
+    eosio::print("Avatar soft deleted (deactivated), ID: ", entityId);
 }
 
 // Inserts avatar detail entity into avatar details list
 [[eosio::action]]
-void NextGenSoftwareOASIS::adddetail(long entityId, string avatarId, string info) 
+void NextGenSoftwareOASIS::adddetail(long entityId, std::string avatarId, std::string info) 
 {
-    // Validating step: check if avatar detail with current id is exist
-    for (auto detailItem : detailsTable) 
-    {
-        if(detailItem.entityId == entityId)
-            return;
-    }
+    eosio::print("Avatar detail creating started...");
 
     // Insert avatar detail entity into avatar detail table
-    avatardetail detailEntity;
-    detailEntity.entityId = entityId;
-    detailEntity.avatarId = avatarId;
-    detailEntity.info = info;
-
-    detailsTable.push_back(detailEntity);
+    _dtls.emplace(get_self(), [&](auto& a) {
+        a.key = _dtls.available_primary_key();
+        a.entityId = entityId;
+        a.avatarId = avatarId;
+        a.info = info;
+    });
 
     // Print insertation result
-    print("New avatar detail created, ID: ", entityId);
-}
-
-// Reads avatar detail entity from avatar details list specified by id
-[[eosio::action]]  
-NextGenSoftwareOASIS::avatardetail NextGenSoftwareOASIS::getdetail(long entityId) 
-{
-    for (auto detailItem : detailsTable) 
-        if(detailItem.entityId == entityId) 
-        {
-            // Print reading result
-            print("Reading avatar detail executed, readed avatar detail with ID: ", detailItem.entityId);
-
-            return detailItem;
-        }   
-
-    print("Reading avatar detail executed, avatar detail not found with ID: ", entityId);
-
-    // if nothing was found
-    return avatardetail{};
-}
-
-// Read all avatar details from avatar details list
-[[eosio::action]]
-NextGenSoftwareOASIS::avatardetail* NextGenSoftwareOASIS::getdetails() 
-{
-    return &detailsTable[0];
+    eosio::print("New avatar detail created, ID: ", entityId);
 }
 
 // Inserts an avatar entity into avatars list
 [[eosio::action]]
-void NextGenSoftwareOASIS::addholon(long entityId, string holonId, string info) 
+void NextGenSoftwareOASIS::addholon(long entityId, std::string holonId, std::string info) 
 {
-    // Validating step: check if holon with current id is exist
-    for (auto holonItem : holonsTable) 
-    {
-        if(holonItem.entityId == entityId)
-            return;
-    }
+    eosio::print("Holon creating started...");
 
     // Insert holon entity into holon table
-    holon holonEntity;
-    holonEntity.entityId = entityId;
-    holonEntity.holonId = holonId;
-    holonEntity.isDeleted = false;
-    holonEntity.info = info;
-
-    holonsTable.push_back(holonEntity);
+    _hlns.emplace(get_self(), [&](auto& h) {
+        h.key = _hlns.available_primary_key();
+        h.entityId = entityId;
+        h.holonId = holonId;
+        h.info = info;
+        h.isDeleted = false;
+    });
 
     // Print insertation result
-    print("New holon created, ID: ", entityId);
-}
-
-// Reads holon entity from holons list specified by its id
-[[eosio::action]]
-NextGenSoftwareOASIS::holon NextGenSoftwareOASIS::getholon(long entityId) 
-{
-    for (auto holonItem : holonsTable) 
-        if(holonItem.entityId == entityId && holonItem.isDeleted == false) 
-        {
-            // Print reading result
-            print("Reading holon executed, readed holon with ID: ", holonItem.entityId);
-
-            return holonItem;
-        }   
-
-    print("Reading holon executed, holon not found with ID: ", entityId);
-
-    // if nothing was found
-    return holon{};
-}
-
-// Read all holons from holons list
-[[eosio::action]]
-NextGenSoftwareOASIS::holon* NextGenSoftwareOASIS::getholons() 
-{
-    return &holonsTable[0];
+    eosio::print("New holon created, ID: ", entityId);
 }
 
 // Updates holon info field by specified holon id
 [[eosio::action]]
 void NextGenSoftwareOASIS::setholon(long entityId, string info) 
 {
-    for (auto holonItem : holonsTable) 
-        if(holonItem.entityId == entityId && holonItem.isDeleted == false) 
-        {
-            holonItem.info = info;
+    eosio::print("Holon updating started...");
 
-            // Print reading result
-            print("Updating completed, Holon ID: ", holonItem.entityId);
-            return;
-        }  
+    std::vector<uint64_t> keysForModify;
+    for(auto& item : _hlns) {
+        if (item.entityId == entityId) {
+            keysForModify.push_back(item.key);   
+        }
+    }
+    
+    for (uint64_t key : keysForModify) {
+        auto itr = _hlns.find(key);
+        if (itr != _hlns.end()) {
+            _hlns.modify(itr, get_self(), [&](auto& h) {
+                h.info = info;
+            });
+        }
+    }
     
     // Print updating result
-    print("Holon not found, updating failed, ID: ", entityId);
+    eosio::print("Holon updated, ID: ", entityId);
 }
 
 // Removes holon entity specified by its entity id from holons list
 [[eosio::action]]
 void NextGenSoftwareOASIS::hardholon(long entityId) 
 {
-    for (auto holonItem : holonsTable) 
-        if(holonItem.entityId == entityId && holonItem.isDeleted == false) 
-        {
-            // TODO: remove holon from the list
-            // avatarTable.erase(remove(avatarTable.begin(), avatarTable.end(), avatarItem), avatarTable.end());
-         
-            // Print reading result
-            print("Holon deleted, ID: ", entityId);
-            return;
-        }  
+    std::vector<uint64_t> keysForDeletion;
+
+    for(auto& item : _hlns) {
+        if (item.entityId == entityId) {
+            keysForDeletion.push_back(item.key);   
+        }
+    }
+    
+    for (uint64_t key : keysForDeletion) {
+        auto itr = _hlns.find(key);
+        if (itr != _hlns.end()) {
+            _hlns.erase(itr);
+        }
+    }
 
     // Print hard-deleting result
-    print("Holon not found, deleting failed, ID: ", entityId);
+    print("Holon hard-deleted, ID: ", entityId);
 }
 
 // Sets holon IsDeleted field value to true specified by its id
 [[eosio::action]]
 void NextGenSoftwareOASIS::softholon(long entityId) 
 {
-    for (auto holonItem : holonsTable) 
-        if(holonItem.entityId == entityId && holonItem.isDeleted == false) 
-        {
-            holonItem.isDeleted = true;
-
-            // Print reading result
-            print("Holon soft delete failed, ID: ", holonItem.entityId);
-            return;
-        }  
+    std::vector<uint64_t> keysForModify;
+    for(auto& item : _hlns) {
+        if (item.entityId == entityId) {
+            keysForModify.push_back(item.key);   
+        }
+    }
+    
+    for (uint64_t key : keysForModify) {
+        auto itr = _hlns.find(key);
+        if (itr != _hlns.end()) {
+            _hlns.modify(itr, get_self(), [&](auto& h) {
+                h.isDeleted = true;
+            });
+        }
+    }
 
     // Print soft-deleting result
-    print("Holon not found, soft deleted failed, ID: ", entityId);
+    print("Holon soft-deleted, ID: ", entityId);
 }

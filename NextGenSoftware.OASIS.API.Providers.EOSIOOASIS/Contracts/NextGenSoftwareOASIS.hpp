4805f7db6e429cecb2fa26459d11d3c64d4ba995
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-05-28 17:54:20 +0500

    EOS smart contract hpp file changed

--- NextGenSoftware.OASIS.API.Providers.EOSIOOASIS/Contracts/NextGenSoftwareOASIS.hpp
+++ NextGenSoftware.OASIS.API.Providers.EOSIOOASIS/Contracts/NextGenSoftwareOASIS.hpp
@@ -1,72 +1,73 @@
+#pragma once
+
 #include <eosio/eosio.hpp>
-#include <vector>
-#include <string>
 
-using namespace std;
-using namespace eosio;
-
-class [[eosio::contract]] NextGenSoftwareOASIS : public contract
+class [[eosio::contract]] NextGenSoftwareOASIS : public eosio::contract
 {
     public:
-        using contract::contract;
-
-        struct avatar
-        {
-            long entityId;
-            string avatarId;
-            string info;
-            bool isDeleted;
-        };
-
-        struct avatardetail
-        {
-            long entityId;
-            string avatarId;
-            string info;
-        };
-
-        struct holon
-        {
-            long entityId;
-            string holonId;
-            string info;
-            bool isDeleted;
-        };
-
-        vector<avatar> avatarTable;
-        vector<avatardetail> detailsTable;
-        vector<holon> holonsTable;
-
+        NextGenSoftwareOASIS( eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds ): eosio::contract(receiver, code, ds),  _avtrs(receiver, code.value), hlnstable(receiver, code.value), dtlstable(receiver, code.value)
+        {}
+        
         [[eosio::action]]
-        void addavatar(long entityId, string avatarId, string info);
+        void addavatar(long entityId, std::string avatarId, std::string info);
         [[eosio::action]]
-        avatar getavatar(long entityId);
-        [[eosio::action]]
-        avatar* getavatars();
-        [[eosio::action]]
-        void setavatar(long entityId, string info);
+        void setavatar(long entityId, std::string info);
         [[eosio::action]]
         void hardavatar(long entityId);
         [[eosio::action]]
         void softavatar(long entityId);
 
         [[eosio::action]]
-        void adddetail(long entityId, string avatarId, string info);
-        [[eosio::action]]
-        avatardetail getdetail(long entityId);
-        [[eosio::action]]
-        avatardetail* getdetails();
+        void adddetail(long entityId, std::string avatarId, std::string info);
 
         [[eosio::action]]
-        void addholon(long entityId, string holonId, string info);
+        void addholon(long entityId, std::string holonId, std::string info);
         [[eosio::action]]
-        holon getholon(long entityId);
-        [[eosio::action]]
-        holon* getholons();
-        [[eosio::action]]
-        void setholon(long entityId, string info);
+        void setholon(long entityId, std::string info);
         [[eosio::action]]
         void hardholon(long entityId);
         [[eosio::action]]
         void softholon(long entityId);
+        
+        struct [[eosio::table]] avatar
+        {
+            uint64_t key;
+            uint64_t entityId;
+            std::string avatarId;
+            std::string info;
+            bool isDeleted;
+            
+            uint64_t primary_key() const { return key; }
+            uint64_t by_entityId() const { return entityId; }
+        };
+        typedef eosio::multi_index<"avatar"_n, avatar, eosio::indexed_by<"avatarid"_n, eosio::const_mem_fun<avatar, uint64_t, &avatar::by_entityId>>> avtrstable;
+
+        struct [[eosio::table]] avatardetail
+        {
+            uint64_t key;
+            uint64_t entityId;
+            std::string avatarId;
+            std::string info;
+            
+            uint64_t primary_key() const { return key; }
+            uint64_t by_entityId() const { return entityId; }
+        };
+        typedef eosio::multi_index<"avatardetail"_n, avatardetail, eosio::indexed_by<"detailid"_n, eosio::const_mem_fun<avatardetail, uint64_t, &avatardetail::by_entityId>>> dtlstable;
+
+        struct [[eosio::table]] holon
+        {
+            uint64_t key;
+            uint64_t entityId;
+            std::string holonId;
+            std::string info;
+            bool isDeleted;
+            
+            uint64_t primary_key() const { return key; }
+            uint64_t by_entityId() const { return entityId; }
+        };
+        typedef eosio::multi_index<"holon"_n, holon, eosio::indexed_by<"holonid"_n, eosio::const_mem_fun<holon, uint64_t, &holon::by_entityId>>> hlnstable;
+        
+        avtrstable _avtrs;
+        dtlstable _dtls;
+        hlnstable _hlns;
 };
